bcd1c564851be5aa2856e1bac45a7e57fab5bb7e
Rafał Krupiński <rafal.krupinski@airspacelink.com>
2023-02-16 13:24:53 +0100

    unify swService API

--- typescript/src/swService.ts
+++ typescript/src/swService.ts
@@ -5,12 +5,14 @@ type Character = {
 let cache: Character;
 
 export const getCharacter = (cb: (data: Character) => void) => {
-  if (cache) return cache;
-
-  fetch("https://swapi.dev/api/people/1")
-    .then((res) => res.json())
-    .then((data) => {
-      cache = data;
-      cb(data);
-    });
+  if (cache) {
+    cb(cache);
+  } else {
+    fetch("https://swapi.dev/api/people/1")
+      .then((res) => res.json())
+      .then((data) => {
+        cache = data;
+        cb(data);
+      });
+  }
 };

7e8f465ed5e73965a1932533aa7ab7b30760c072
Shane Jaroch <chown_tee@proton.me>
2023-10-26 12:25:03 -0400

    better

--- typescript/src/swService.ts
+++ typescript/src/swService.ts
@@ -4,24 +4,28 @@ type Character = {
 
 let cache: Character;
 
-export const getCharacter = (_callBack: (data: Character) => void) => {
-  if (cache) {
-    _callBack(cache);
+// TODO: no tests (for getCharacter), advised to add unit testing framework
 
+export const getCharacter = (): Promise<Character> => {
+  if (cache) {
+    console.info("Using Cache!")
+    return cache;
   } else {
-    // Fetch the character, since cache is lacking
-    fetch("https://swapi.dev/api/people/1")
+    return fetch("https://swapi.dev/api/people/1")
       .then((res) => res.json())
       .then((data) => {
-        cache = data;  // TODO: validate typing, this just stuffs JSON into Character
+        cache = data;
+
+        // NOTE: that this occurs asyncrhonously now (after Bravo log)
         console.info("response data: " + JSON.stringify(data));
 
         // Test that data meets our contract
-        if (typeof(data["name"]) !== 'string') {
+        // TODO: add better type checking (is name only requirement?  Or other properties?)
+        if (typeof (data["name"]) !== 'string') {
           throw new Error("'Name' property missing.");
         };
 
-        _callBack(data);
+        return data;
       }).catch(error => {
         console.error("Failed to retrieve data from API: " + error.stack);
         throw new Error(error);

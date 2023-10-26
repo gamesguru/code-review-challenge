63e0d3a60984411400684b14821afa3da9c59006
Shane Jaroch <chown_tee@proton.me>
2023-10-25 21:04:27 -0400

    update

--- typescript/src/swService.ts
+++ typescript/src/swService.ts
@@ -4,15 +4,27 @@ type Character = {
 
 let cache: Character;
 
-export const getCharacter = (cb: (data: Character) => void) => {
+export const getCharacter = (_callBack: (data: Character) => void) => {
   if (cache) {
-    cb(cache);
+    _callBack(cache);
+
   } else {
+    // Fetch the character, since cache is lacking
     fetch("https://swapi.dev/api/people/1")
       .then((res) => res.json())
       .then((data) => {
-        cache = data;
-        cb(data);
+        cache = data;  // TODO: validate typing, this just stuffs JSON into Character
+        console.info("response data: " + JSON.stringify(data));
+
+        // Test that data meets our contract
+        if (typeof(data["name"]) !== 'string') {
+          throw new Error("'Name' property missing.");
+        };
+
+        _callBack(data);
+      }).catch(error => {
+        console.error("Failed to retrieve data from API: " + error.stack);
+        throw new Error(error);
       });
   }
 };
